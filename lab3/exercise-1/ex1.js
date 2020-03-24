2d203b3bd62932d7ce001a2ee71066132f4d46be
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-03-25 04:04:02 +0430

    ex1 done!

---
+++ lab3/exercise-1/ex1.js
@@ -0,0 +1,12 @@
+let uc = require('upper-case')
+
+console.log(uc.upperCase('string'))
+
+
+const greet = () => {
+    for ( var i = 0;i<10;i++){
+        console.log(uc.upperCase('hello world'));
+    }
+}
+
+greet();
