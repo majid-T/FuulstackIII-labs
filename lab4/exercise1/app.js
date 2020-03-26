09de8164e61ddbeb5dc800dd80709f48ace84e67
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-03-27 03:02:24 +0430

    Lab4 exe1 done!

---
+++ lab4/exercise1/app.js
@@ -0,0 +1,6 @@
+const ft = require('./sportsteam');
+
+ft.teamMate = 'Leaf';
+
+ft.Cheer();
+ft.Boo();
---
+++ lab4/exercise1/sportsteam/index.js
@@ -0,0 +1,8 @@
+exports.Cheer = function(){
+    console.log(`Go! ${this.teamMate} Go!`);
+}
+
+exports.Boo = function(){
+    console.log(`Booooooo! ${this.teamMate} Booooooo!`);
+}
+
